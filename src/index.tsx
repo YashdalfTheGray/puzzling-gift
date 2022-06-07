a62349b4a8f6ca1061bf0f16e51c80b9953c6dd2
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-06-07 06:53:07 -0700

    stable parcel build to start with

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import { render, FunctionalComponent } from 'preact';
 
+import { initFirebaseApp } from './firebase';
+
+initFirebaseApp();
+
 const App: FunctionalComponent<{}> = () => (
   <div>This is where the app will go</div>
-)
+);
 
 document.querySelector('div#app-root')!.innerHTML = '';
 render(<App />, document.querySelector('div#app-root')!);
