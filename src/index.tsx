d926d80dcae809e2d272c70c1490ccd50e6953bc
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-06-08 09:05:37 -0700

    replaced in the app component rather than inline creating one

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
 import { render, FunctionalComponent } from 'preact';
 
-import { initFirebaseApp } from './firebase';
+import { initFirebaseApp } from '~/src/firebase';
+import App from '~/src/Components/App';
 
 initFirebaseApp();
-
-const App: FunctionalComponent<{}> = () => (
-  <div>This is where the app will go</div>
-);
 
 document.querySelector('div#app-root')!.innerHTML = '';
 render(<App />, document.querySelector('div#app-root')!);
