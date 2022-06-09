e7a2ef78ebb958beafe8e193ea172b3aab9ace13
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-06-09 01:28:20 -0700

    added a footer component for credits and info

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,24 @@
+import { Component } from 'preact';
+
+import './Footer.scss';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        Designed and developed in the open by &nbsp;
+        <a href="https://yashdalfthegray.github.io/" target="blank">
+          Yash
+        </a>
+        . &nbsp;The code can be found &nbsp;
+        <a
+          href="https://github.com/YashdalfTheGray/puzzling-gift"
+          target="blank"
+        >
+          here
+        </a>
+        .
+      </div>
+    );
+  }
+}
