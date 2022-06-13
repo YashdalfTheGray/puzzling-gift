b5991173886f3d17b5e3da8219e62b912a3436ba
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-06-12 23:20:43 -0700

    redid the header component and used mui elements

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,21 +1,75 @@
 import { Component } from 'preact';
 
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+
 import { doAuthRedirect } from '~/src/auth';
-import Title from '~/src/components/Title';
 
-import './Header.scss';
+export type HeaderState = {
+  menuAnchorElement: HTMLElement | null;
+};
 
-export default class Header extends Component {
+export default class Header extends Component<{}, HeaderState> {
+  constructor() {
+    super();
+    this.state = {
+      menuAnchorElement: null,
+    };
+  }
+
+  public handleMenuOpen = (event: MouseEvent) => {
+    this.setState({ menuAnchorElement: event.currentTarget as HTMLElement });
+  };
+
+  public handleMenuClose = (event: MouseEvent) => {
+    this.setState({ menuAnchorElement: null });
+  };
+
   render() {
+    const { menuAnchorElement } = this.state;
+
     return (
-      <div className="header">
-        <Title type="h1">Puzzle Tracker</Title>
-        <span
-          className="material-symbols-outlined login-icon"
-          onClick={doAuthRedirect}>
-          login
-        </span>
-      </div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Puzzle Tracker
+          </Typography>
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={this.handleMenuOpen}
+              color="inherit">
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={menuAnchorElement}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(menuAnchorElement)}
+              onClose={this.handleMenuClose}>
+              <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
+              <MenuItem onClick={this.handleMenuClose}>My account</MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
