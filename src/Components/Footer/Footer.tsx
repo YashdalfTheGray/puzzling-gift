d6a8601b439053355160452fb0114ce982c5622b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-06-13 23:25:03 -0700

    changed the footer to be more material like

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,20 +1,21 @@
 import { Component } from 'preact';
 
+import Paper from '@mui/material/Paper';
+
 import './Footer.scss';
 
 export default class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        Designed and developed in the open by &nbsp;
+        Designed and developed in the open by&nbsp;
         <a href="https://yashdalfthegray.github.io/" target="blank">
           Yash
         </a>
-        . &nbsp;The code can be found &nbsp;
+        .&nbsp;The code can be found&nbsp;
         <a
           href="https://github.com/YashdalfTheGray/puzzling-gift"
-          target="blank"
-        >
+          target="blank">
           here
         </a>
         .
