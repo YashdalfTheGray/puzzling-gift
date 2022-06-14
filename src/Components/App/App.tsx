f326f873ba12610f28416a62561d874eef346429
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-06-13 23:25:22 -0700

    integrated the app component with material ui

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,18 +1,31 @@
 import { Component } from 'preact';
 
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+
+import { getAuthResult } from '~/src/auth';
 import Header from '~/src/Components/Header';
 import Footer from '~/src/Components/Footer';
 
+import { theme } from '~/src/theme';
 import './App.scss';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    getAuthResult();
+  }
   render() {
     return (
-      <div className="app">
-        <Header />
-        <div>this is where the content will go</div>
-        <Footer />
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline enableColorScheme={true} />
+        <div className="app">
+          <Header />
+          <div>This is where the main content will go</div>
+          <Footer />
+        </div>
+      </ThemeProvider>
     );
   }
 }
