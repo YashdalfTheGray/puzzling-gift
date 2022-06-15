da25b71fdf00f871252e0a0e08601b4d49aa28b8
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-06-14 22:13:10 -0700

    added some redux utils to make dealing with it a bit easier

---
+++ src/redux/utils.ts
@@ -0,0 +1,47 @@
+import { combineReducers } from 'redux';
+
+export interface IAction<T extends string> {
+  type: T;
+}
+
+export interface IActionWithPayload<T extends string, P> extends IAction<T> {
+  payload: P;
+}
+
+export type ActionsMap<A extends { [key: string]: (...args: any[]) => any }> =
+  ReturnType<A[keyof A]>;
+
+export function createAction<T extends string>(type: T): IAction<T>;
+export function createAction<T extends string, P>(
+  type: T,
+  payload: P
+): IActionWithPayload<T, P>;
+export function createAction<T extends string, P>(type: T, payload?: P) {
+  return { type, payload };
+}
+
+export interface IApiProcessingSlice {
+  isProcessing: boolean;
+  apiError: Error;
+}
+
+export const createProcessingSlice = (
+  apiAction: string,
+  successAction: string,
+  errorAction: string
+) => {
+  const isProcessing = (state = false, action: any) => {
+    if (action.type === apiAction) {
+      return true;
+    }
+    if (action.type === successAction || action.type === errorAction) {
+      return false;
+    }
+    return state;
+  };
+
+  const apiError = (state = null, action: any) =>
+    action.type === errorAction ? action.payload : state;
+
+  return combineReducers({ isProcessing, apiError });
+};
