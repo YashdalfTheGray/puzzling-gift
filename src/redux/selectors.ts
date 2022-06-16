2564d69c65b993b58f135a27889899d738008336
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-06-15 23:53:08 -0700

    added some data selectors to filter the store with

---
+++ src/redux/selectors.ts
@@ -0,0 +1,20 @@
+import { PuzzleStore } from './puzzleStore';
+
+export const getPuzzleSetById = (state: PuzzleStore, id: string) =>
+  state.puzzleSets[id];
+export const getPuzzleById = (state: PuzzleStore, id: string) =>
+  state.puzzles[id];
+export const getPuzzleClueByIdAndIndex = (
+  state: PuzzleStore,
+  id: string,
+  index: number
+) => state.puzzleClues[id][index];
+export const getPuzzleSolutionByIdAndIndex = (
+  state: PuzzleStore,
+  id: string,
+  index: number
+) => state.puzzleSolutions[id][index];
+export const getActionErrorByAction = (state: PuzzleStore, api: string) =>
+  state[`${api}ActionProcessing`].ActionError as Error;
+export const getIsProcessingByAction = (state: PuzzleStore, api: string) =>
+  state[`${api}ActionProcessing`].isProcessing as boolean;
