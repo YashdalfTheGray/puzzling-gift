fe680eeaf62788704d48761282c5bde3ccee28bd
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-06-17 22:07:12 -0400

    added the redux store to the app using the provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
-import { render, FunctionalComponent } from 'preact';
+import { render } from 'preact';
+import { Provider } from 'react-redux';
 
 import { initFirebaseApp } from '~/src/firebase';
+import store from '~/src/redux';
 import App from '~/src/components/App';
 
 initFirebaseApp();
 
+const ProvidedApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
 document.querySelector('div#app-root')!.innerHTML = '';
-render(<App />, document.querySelector('div#app-root')!);
+render(<ProvidedApp />, document.querySelector('div#app-root')!);
