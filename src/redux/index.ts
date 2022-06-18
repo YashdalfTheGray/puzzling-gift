bb40b89bc40fcaa68da6da4ce2092b07318e7adf
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-06-17 22:06:51 -0400

    wired up the sagas as well as the reducers properly

---
+++ src/redux/index.ts
@@ -0,0 +1,17 @@
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import createSagaMiddleware from 'redux-saga';
+
+import reducer from './reducers';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
