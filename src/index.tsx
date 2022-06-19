aff384c260f22c8b40360f3dc9c346148b4e7f82
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-06-19 14:26:21 -0400

    switched to react-dom/client and create root rather than render

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 // Can't use preact because https://github.com/parcel-bundler/parcel/issues/7867
 // import { render } from 'preact';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { initFirebaseApp } from '~/src/firebase';
@@ -15,5 +15,6 @@ const ProvidedApp = () => (
   </Provider>
 );
 
-document.querySelector('div#app-root')!.innerHTML = '';
-ReactDOM.render(<ProvidedApp />, document.querySelector('div#app-root')!);
+const appRoot = document.querySelector('div#app-root')!;
+const root = createRoot(appRoot);
+root.render(<ProvidedApp />);
